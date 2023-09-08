f1fdba88f7a5dbcc273ff9118cbeff561ddb35e0
Yasmin Ledesma <yledesma@alquilerargentina.com>
2023-09-07 21:56:55 -0300

    feat(bbs): add solution for bubble sort

--- src/day1/BubbleSort.ts
+++ src/day1/BubbleSort.ts
@@ -1,3 +1,14 @@
 export default function bubble_sort(arr: number[]): void {
+    let lastIdx = arr.length;
 
+    while (lastIdx > 0) {
+        for (let i = 0; i < lastIdx; i++) {
+            if (arr[i] > arr[i + 1]) {
+                const tmp = arr[i]
+                arr[i] = arr[i + 1]
+                arr[i + 1] = tmp
+            }
+        }
+        lastIdx -= 1
+    }
 }
