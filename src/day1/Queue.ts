d44941acdffe0e010db3721f417f8787a29604d1
Yasmin Ledesma <yledesma@alquilerargentina.com>
2023-09-11 23:37:36 -0300

    feat(queue): add solution for queue

--- src/day1/Queue.ts
+++ src/day1/Queue.ts
@@ -1,18 +1,47 @@
+type Node<T> = {
+    curr: T,
+    prev?: Node<T>
+}
+
 export default class Queue<T> {
     public length: number;
+    private head?: Node<T>
+    private tail?: Node<T>
 
-    
 
     constructor() {
+        this.head = this.tail = undefined
+        this.length = 0
+
     }
 
     enqueue(item: T): void {
+        const node: Node<T> = { curr: item, prev: undefined }
 
-}
+        if (this.length === 0) {
+            this.head = this.tail = node
+            this.tail.prev = node
+            this.length++
+            return
+        }
+
+        this.head ? this.head.prev = node : null
+        this.head = node
+        this.length++
+        return
+    }
+
     deque(): T | undefined {
+        if (this.length === 0) return
 
-}
+        const tail = this.tail?.curr
+        this.tail = this.tail?.prev
+        this.length--
+
+        return tail
+    }
+
     peek(): T | undefined {
-
-}
+        return this.tail?.curr
+    }
 }
