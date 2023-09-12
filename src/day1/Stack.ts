2fa148e3d7d3b8ec780a59d1dd9f2ca10a41a93a
Yasmin Ledesma <yledesma@alquilerargentina.com>
2023-09-12 20:44:36 -0300

    feat(stack): add solution for stack

--- src/day1/Stack.ts
+++ src/day1/Stack.ts
@@ -1,18 +1,42 @@
+type Node<T> = {
+    curr: T,
+    next?: Node<T>
+}
 export default class Stack<T> {
     public length: number;
-
-    
+    private head?: Node<T>
 
     constructor() {
+        this.head = undefined
+        this.length = 0
     }
 
     push(item: T): void {
+        const node: Node<T> = { curr: item, next: undefined }
+        this.length++
 
-}
+        if (!this.head) {
+            this.head = node
+            return
+        }
+
+        node.next = this.head
+        this.head = node
+        return
+    }
+
     pop(): T | undefined {
+        const val = this.head?.curr
+        this.head = this.head?.next
 
-}
+        if (this.length > 0) {
+            this.length--
+        }
+
+        return val
+    }
+
     peek(): T | undefined {
-
-}
+        return this.head?.curr
+    }
 }
