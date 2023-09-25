d170902f59fcacc5df6e8806c6a1075fd2da2554
Yasmin Ledesma <72028038+yasledesma@users.noreply.github.com>
2023-09-24 23:53:09 -0300

    feat(ms): add solution for Maze Solver
    Definitely not a good solution to mazes that can branch in all sides.

--- src/day1/MazeSolver.ts
+++ src/day1/MazeSolver.ts
@@ -1,3 +1,24 @@
-export default function solve(maze: string[], wall: string, start: Point, end: Point): Point[] {
+const points: Point[] = []
 
+export default function solve(maze: string[], wall: string, start: Point, end: Point): Point[] {
+    // base
+    if (start.y === end.y && start.x === end.x) {
+        points.push(start)
+        return points
+    }
+
+    points.push(start)
+    return solve(maze, wall, getNextPoint(maze, points[points.length - 1], wall), end)
+}
+
+function getNextPoint(maze: string[], point: Point, wall: string): Point {
+    if (maze[point.y][point.x + 1] !== wall && !points.filter(p => p.x === (point.x + 1) && p.y === point.y)[0]) {
+        return { x: point.x + 1, y: point.y }
+    }
+
+    if (maze[point.y][point.x - 1] !== wall && !points.filter(p => p.x === (point.x - 1) && p.y === point.y)[0]) {
+        return { x: point.x - 1, y: point.y }
+    }
+
+    return { x: point.x, y: point.y + 1 }
 }
